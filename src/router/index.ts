33729d94d057f9a41692bce119c73b8c41d108f5
周涛 <1055294203@qq.com>
2023-08-03 11:04:52 +0800

    实现markdown

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
+import { h } from "vue";
 import { createWebHistory, createRouter } from "vue-router";
+
 import Home from "@/views/home/index.vue";
 import Doc from "@/views/doc/index.vue";
 import SwitchDemo from "@/components/SwitchDemo.vue";
 import ButtonDemo from "@/components/ButtonDemo.vue";
 import DialogDemo from "@/components/DialogDemo.vue";
 import TabsDemo from "@/components/TabsDemo.vue";
+
+import Markdown from "@/components/Markdown.vue";
+const md = (filename) =>
+  h(Markdown, { path: `../../markdown/${filename}.md`, key: filename });
 
 const history = createWebHistory();
 const router = createRouter({
@@ -16,22 +22,13 @@ const router = createRouter({
       path: "/doc",
       component: Doc,
       children: [
-        {
-          path: "switch",
-          component: SwitchDemo,
-        },
-        {
-          path: "button",
-          component: ButtonDemo,
-        },
-        {
-          path: "dialog",
-          component: DialogDemo,
-        },
-        {
-          path: "tabs",
-          component: TabsDemo,
-        },
+        { path: "intro", component: md("intro") },
+        { path: "install", component: md("install") },
+        { path: "get-started", component: md("get-started") },
+        { path: "switch", component: SwitchDemo },
+        { path: "button", component: ButtonDemo },
+        { path: "dialog", component: DialogDemo },
+        { path: "tabs", component: TabsDemo },
       ],
     },
   ],
