781a5a537c236db7fbc3c7e455415657afecec44
zhoutao <1055294203@qq.com>
2023-08-09 10:43:31 +0800

    feat:完善Switch文档

--- src/lib/switch/index.vue
+++ src/lib/switch/index.vue
@@ -3,7 +3,7 @@
     class="rosy-switch"
     :class="classes"
     @click="toggle"
-    :style="{ background: modelValue ? inactiveColor : activeColor }"
+    :style="{ background: modelValue ? activeColor : inactiveColor }"
   >
     <span></span>
   </button>
@@ -18,11 +18,11 @@ const props = defineProps({
   },
   activeColor: {
     type: String,
-    default: "",
+    default: "#18a058",
   },
   inactiveColor: {
     type: String,
-    default: "",
+    default: "#dbdbdb",
   },
   disabled: {
     type: Boolean,
--- src/views/doc/switch/index.vue
+++ src/views/doc/switch/index.vue
@@ -1,15 +1,31 @@
 <template>
   <h1>Switch 示例</h1>
-  <Preview :component="SwitchPreview1" />
-  <Preview :component="SwitchPreview2" />
-  <Preview :component="SwitchPreview3" />
+  <div class="preview-wrapper">
+    <Preview :component="SwitchPreview1" />
+    <Preview :component="SwitchPreview2" />
+    <Preview :component="SwitchPreview3" />
+    <Doc title="属性" type="prop" :body="propsDoc" />
+    <PrevAndNext :prev="{ path: '/doc/button', name: 'Button按钮' }" :next="{ path: '/doc/dialog', name: 'Dialog对话框' }" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import Doc from "@/components/Doc.vue";
+import PrevAndNext from "@/components/PrevAndNext.vue";
 import Preview from "@/components/Preview.vue";
 import SwitchPreview1 from "./SwitchPreview1.preview.vue";
 import SwitchPreview2 from "./SwitchPreview2.preview.vue";
 import SwitchPreview3 from "./SwitchPreview3.preview.vue";
+
+
+const propsDoc = [
+["v-model", "绑定值", "boolean", "-", "false"],
+  ["active-color", "在开启状态时的背景颜色", "string", "-", "#18a058"],
+  ["inactive-color", "在关闭状态时的背景颜色", "string", "-", "#dbdbdb"],
+  ["disabled", "是否禁用", "boolean", "-", "false"],
+  ["size", "大小", "string", "small / default / large", "default"],
+];
+
 </script>
 
 <style lang="scss" />
